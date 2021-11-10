686fb1fc0e56f8e7ce649b83c610f0c6eff33fcd
kuryama <kuriyama@dcinc.co.jp>
2021-11-10 13:57:49 +0900

    アイコンを設定、ビルドのテンプレートを修正

--- install-templates.cmd
+++ install-templates.cmd
@@ -3,9 +3,10 @@ rem uninstall templates
 dotnet new --uninstall NextDesign.Extension.ProjectTemplates
 
 rem pack 
-dotnet pack src -o ./NuPkgs
+del .\src\bin\Release\*.nupkg
+dotnet pack src -o ./src/bin/Release
 
 rem install templates
-dotnet new --install ./NuPkgs/*.nupkg
+dotnet new --install ./src/bin/Release/*.nupkg
 
 pause
